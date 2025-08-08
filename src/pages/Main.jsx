eef0699f7d12753ca387ff6fd01b308641242466
Goga-Chkhaidze <chxaidze.goga@list.ru>
2025-08-08 20:22:15 +0400

    dsa

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -73,6 +73,193 @@ function Main() {
       </div>
     </div>
 
+    <div className="container-1800">
+      <div className="caseFlex">
+        <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Plague Case</h2>
+          <img src="/images/case4.png" alt="" className="caseImage" />
+          <p className="price">83.00$</p>
+        </div>
+        <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Ninja Case</h2>
+          <img src="/images/case5.png" alt="" className="caseImage" />
+          <p className="price">83.00$</p>
+        </div>
+        <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Cursed Case</h2>
+          <img src="/images/case2.png" alt="" className="caseImage" />
+          <p className="price">83.00$</p>
+        </div>
+        <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Gemstone Case</h2> 
+          <img src="/images/case3.png" alt="" className="caseImage" />
+          <p className="price">83.00$</p>
+        </div>
+        <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Neon Case</h2>
+          <img src="/images/case1.png" alt="" className="caseImage" />
+          <p className="price">83.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p> className="price"00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+        </div>
+        <img src="/images/background.png" className="background" alt="" />
+        <div className="caseFlex">
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+                <div className="w_20">
+          <span className="edgeLeft"></span>
+          <span className="edgeRight"></span>
+          <h2 className="caseTitle">Unknown Case</h2>
+          <img src="/images/case0.png" alt="" className="caseImage" />
+          <p className="price">00.00$</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="footer">
+      <div className="container-1800-2">
+        <div className="footerFlex">
+      <div className="w-25-2 z">
+        <div className="logoDivv">
+          <img src="images/logoo.png" alt="" className="footerLogo" />
+          <h1 className="footerTitles h1">CaseSpinner</h1>
+          </div>
+          <h3 className="icons i"><FaFacebookF /></h3>
+          <h3 className="icons i"><FaTwitter /></h3>
+          <h3 className="icons i"><FaInstagram /></h3>
+          <h3 className="icons i"><FaDiscord /></h3>
+      </div>
+      <div className="w-25-2">
+        <h2 className="footerTitles pt">Navigation</h2>
+        <p className="footerText">Cases</p>
+        <p className="footerText">Upgrader</p>
+        <p className="footerText">Exchanger</p>
+      </div>
+      <div className="w-25-2">
+        <h2 className="footerTitles pt">Information</h2>
+        <p className="footerText">Terms of service</p>
+        <p className="footerText">Privacy policy</p>
+        <p className="footerText">About cases</p>
+      </div>
+      <div className="w-25-2">
+        <h2 className="footerTitles pt">Help</h2>
+        <p className="footerText">Provably Fair</p>
+        <p className="footerText">Take a breack</p>
+        <p className="footerText">Support</p>
+      </div>
+      </div>
+      </div>
+            <div className="lineDivv"><span className="middleLine"></span></div>
+            <div className="container-1800-3">
+              <h2 className="copyright">Copyright © 2025</h2>
+            </div>
+    </div>
+
     </>
   )
 }
